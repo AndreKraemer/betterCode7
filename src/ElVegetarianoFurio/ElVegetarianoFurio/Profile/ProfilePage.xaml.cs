6eb49f4d655e4f69c8c47cc77311eb070e054332
André Krämer <andre@andrekraemer.de>
2022-11-24 01:13:54 +0100

    Übung 03 -  Ende

--- src/ElVegetarianoFurio/ElVegetarianoFurio/Profile/ProfilePage.xaml.cs
+++ src/ElVegetarianoFurio/ElVegetarianoFurio/Profile/ProfilePage.xaml.cs
@@ -2,8 +2,18 @@ namespace ElVegetarianoFurio.Profile;
 
 public partial class ProfilePage : ContentPage
 {
+
+	private ProfileViewModel _viewModel;
+
 	public ProfilePage()
 	{
 		InitializeComponent();
+		BindingContext = _viewModel = new ProfileViewModel();
 	}
+
+    protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+		await _viewModel.Initialize();
+        base.OnNavigatedTo(args);
+    }
 }
---
+++ src/ElVegetarianoFurio/ElVegetarianoFurio/Profile/ProfileViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ElVegetarianoFurio.Profile;
+
+[INotifyPropertyChanged]
+public partial class ProfileViewModel
+{
+
+    [ObservableProperty]
+    private string _city = string.Empty;
+
+    [ObservableProperty]
+    private string _fullName = string.Empty;
+
+    [ObservableProperty]
+    private string _phone = string.Empty;
+
+    [ObservableProperty]
+    private string _street = string.Empty;
+
+    [ObservableProperty]
+    private string _zip = string.Empty;
+
+    [ObservableProperty]
+    private bool _isBusy;
+
+    public async Task Initialize()
+    {
+        IsBusy = true;
+        await Task.Delay(3000);
+        IsBusy = false;
+    }
+
+    [RelayCommand(CanExecute = nameof(CanSave))]
+    private async Task SaveAsync()
+    {
+        IsBusy = true;
+        await Task.Delay(3000);
+        IsBusy = false;
+    }
+
+    private bool CanSave()
+    {
+        return !IsBusy;
+    }
+}
